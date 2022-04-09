5b06788b704167d873474e906f4c1a98c261abf3
Johnhughes814 <8hughes.john@gmail.com>
2022-04-09 18:41:28 -0500

    server setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const inquirer = require("inquirer")
+const mysql = require("mysql2")
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password:"NEW_USER_PASSWORD",
+    database: "employee_trackerDB"
+  });
+
+connection.connect(function(err) {
+    if (err) throw err
+    console.log("Connected as Id" + connection.threadId)
+    // startPrompt();
+});
